09ef23276851d3bf471bf458139b697d65fde3d1
KABAHINDA <kabahindarovence@gmail.com>
2020-09-22 08:02:58 +0300

    [Finishes #174848257,#174848755]

--- receipt js/orderform.js
+++ receipt js/orderform.js
@@ -1,16 +1,25 @@
-    
-//validation for the right email format
-function validateemail() {
-var emailID = document.orderform.email.value;
-let x = emailID.indexOf("@");
-let y = emailID.lastIndexOf(".");
 
-if (y < 1 || ( y - x < 2 )) {
- alert("Please enter correct email ID")
- document.orderform.email.focus() ;
- return false;
-}
-return( true );
+/*Validatation for the right email address*/
+var pageDetails = document.querySelector("#page-details"),
+var form = document.querySelector("form");
+
+function validateemail(e) {
+    e.preventDefault();
+    var emailID = document.orderform.email.value;
+    let x = emailID.indexOf("@");
+    let y = emailID.lastIndexOf(".")
+
+    if (y < 1 || (y - x < 2)) {
+        //allert("Please enter correct email ID")
+        var h3 = document.createElement("h3");
+        var mes = document.createTextNode("please enter correct email ID");
+        h3.appendChild(mes);
+        pageDetails.insertBefore(h3, form);
+        document.orderform.email.focus();
+        return false;
+
+    }
+    return (true);
 }
 
 
