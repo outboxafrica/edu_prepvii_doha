c9a30d0653f71ed435e3379c5b21c614ef642538
Muwanguzi Peter <petermuwanguzi51@gmail.com>
2020-09-22 22:15:11 -0700

    just a code pretier

--- data/users.js
+++ data/users.js
@@ -1,14 +1,14 @@
 var data = [
-    {
-        "username":"MUWANGUZI",
-        "email":"muwanguzi51@gmail.com",
-        "password":"peter12346"
-    },
-    {
-        "username":"PETER",
-        "email":"petermuwanguzi51@gmail.com",
-        "password":"peter1234"
-    }
-]
+  {
+    username: "MUWANGUZI",
+    email: "muwanguzi51@gmail.com",
+    password: "peter12346",
+  },
+  {
+    username: "PETER",
+    email: "petermuwanguzi51@gmail.com",
+    password: "peter1234",
+  },
+];
 
-localStorage.setItem('data',JSON.stringify(data));
+localStorage.setItem("data", JSON.stringify(data));
--- receipt js/orderform.js
+++ receipt js/orderform.js
@@ -1,20 +1,17 @@
-  var totalPrice = JSON.parse(localStorage.getItem('totalPrice'));
-  console.log(totalPrice);
-  document.querySelector("#total").innerHTML=totalPrice;
- 
-   
+var totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
+console.log(totalPrice);
+document.querySelector("#total").innerHTML = totalPrice;
+
 //validation for the right email format
 function validateemail() {
-var emailID = document.orderform.email.value;
-let x = emailID.indexOf("@");
-let y = emailID.lastIndexOf(".");
+  var emailID = document.orderform.email.value;
+  let x = emailID.indexOf("@");
+  let y = emailID.lastIndexOf(".");
 
-if (y < 1 || ( y - x < 2 )) {
- alert("Please enter correct email ID")
- document.orderform.email.focus() ;
- return false;
+  if (y < 1 || y - x < 2) {
+    alert("Please enter correct email ID");
+    document.orderform.email.focus();
+    return false;
+  }
+  return true;
 }
-return( true );
-}
-
-
