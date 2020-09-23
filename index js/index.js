d567050eeaedc0443548941c59dc77c56495157d
MichaelKasingye <michaelkasingye@gmail.com>
2020-09-23 06:29:54 -0700

    ft:change paths

--- index js/index.js
+++ index js/index.js
@@ -3,8 +3,8 @@ var Sign_Up = document.querySelector(".Sign_Up");
 
 console.log(login);
 login.addEventListener('click', function(){
-    window.location.assign("./UI/login.html");
+    window.location.assign("UI/login.html");
 });
 Sign_Up.addEventListener('click', function(){
-    window.location.assign("./UI/createAccount.html");
+    window.location.assign("UI/createAccount.html");
 });
