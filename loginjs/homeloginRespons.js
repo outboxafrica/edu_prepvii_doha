b6dd3831cccc9b8b06cf2c0983afeedb34a32f6c
Ghost543 <48438017+Ghost543@users.noreply.github.com>
2020-09-24 12:23:53 -0700

    Update homeloginRespons.js
    fixed typos for homelogin js

--- loginjs/homeloginRespons.js
+++ loginjs/homeloginRespons.js
@@ -1,4 +1,4 @@
 var user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
 
-document.querySelector("#suc").innerHTML = `Welcom ${user.username}`;
+document.querySelector("#suc").innerHTML = `Welcome ${user.username}`;
